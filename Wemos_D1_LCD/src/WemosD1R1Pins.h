ac128c9df5f98509ebece9c9e6357928490b118f
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-10 17:14:20 +0200

    Add new wemos pin lib version

---
+++ Wemos_D1_LCD/src/WemosD1R1Pins.h
@@ -0,0 +1,32 @@
+/*
+The correct sequence of pins Wemos D1 similar to Arduino UNO
+*/
+
+//link D-pins to GPIO for version R1
+#define D0 3
+#define RX D0
+
+#define D1 1
+#define TX D1
+
+#define D2 16
+
+#define D3 5
+
+#define D4 4
+
+#define D5 14
+
+#define D6 12
+
+#define D7 13
+
+#define D8 0
+
+#define D9 2
+#define BOARD_LED D9
+
+#define D10 15
+
+//link A-pin
+#define A0 0
