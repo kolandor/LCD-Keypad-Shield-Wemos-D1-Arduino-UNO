4f56181383aee205637c3c7da1abdc167246bef9
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-10 18:55:15 +0200

    Add LcdKeypadShield handle class

---
+++ Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.cpp
@@ -0,0 +1,11 @@
+#include "LcdKeypadShield.h"
+#include <LiquidCrystal.h>
+
+LcdKeypadShield::LcdKeypadShield(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+{
+    
+}
+
+LcdKeypadShield::~LcdKeypadShield()
+{
+}
---
+++ Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.h
@@ -0,0 +1,8 @@
+class LcdKeypadShield
+{
+private:
+    /* data */
+public:
+    LcdKeypadShield(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+    ~LcdKeypadShield();
+};
