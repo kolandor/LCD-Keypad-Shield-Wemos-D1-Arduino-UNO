959a57b3676fb0fd0420e96f871cfe4e82d58c0f
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-10 19:20:23 +0200

    Add lcd initializer

--- Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.cpp
+++ Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.cpp
@@ -1,7 +1,6 @@
 #include "LcdKeypadShield.h"
-#include <LiquidCrystal.h>
 
-LcdKeypadShield::LcdKeypadShield(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+LcdKeypadShield::LcdKeypadShield(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) : lcd(rs, enable, d4, d5, d6, d7)
 {
     
 }
--- Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.h
+++ Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.h
@@ -1,8 +1,14 @@
+#include <LiquidCrystal.h>
+
 class LcdKeypadShield
 {
 private:
-    /* data */
+
+    LiquidCrystal lcd;
+
 public:
+
     LcdKeypadShield(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+
     ~LcdKeypadShield();
 };
