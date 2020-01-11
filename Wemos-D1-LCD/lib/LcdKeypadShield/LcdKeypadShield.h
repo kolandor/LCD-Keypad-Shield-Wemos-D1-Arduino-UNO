57f1f89e79d14ef0d1a8432f524f4fa17335ae89
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-11 21:19:07 +0200

    Add methods prototypes

--- Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.h
+++ Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.h
@@ -1,9 +1,20 @@
 #include <LiquidCrystal.h>
 #include "BoardType.h"
+#include "Button.h"
 
 class LcdKeypadShield : public LiquidCrystal
 {
 private:
+
+    /*The number of buttons on the shield*/
+    const static int _buttonsCount = 5;
+
+    /*This variable takes into account the possible 
+    unstable current value when reading the analog button*/
+    const int _resistorCurrentDeviation = 25;
+    
+    //Buttons analog values
+    int _buttonsValues[_buttonsCount]{0};
 
 public:
     //Arbitrary connection of pins to LCD
@@ -12,6 +23,15 @@ public:
 
     ~LcdKeypadShield();
 
-    //Connecting the shield directly to the board
+    /*This method can be used if the shield display is connected directly to the board. 
+    The detode will automatically initialize the display for your board*/
     static LcdKeypadShield createByBoaard(const BoardType &board);
+
+    bool isAvailableButton();
+
+    Button getButton();
+
+    void setButtonValue(Button button, int analogButtonValue);
+
+    void userButtonsAutoCorrect();
 };
