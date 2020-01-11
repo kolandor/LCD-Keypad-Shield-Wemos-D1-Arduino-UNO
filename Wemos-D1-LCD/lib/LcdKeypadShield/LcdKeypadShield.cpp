20bfd7095be14fea37b76e7ac11a62760743355a
alex.anipchenko <alex.anipchenko@gmail.com>
2020-01-11 14:07:17 +0200

    Add easy connection board constructor

--- Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.cpp
+++ Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.cpp
@@ -1,10 +1,29 @@
 #include "LcdKeypadShield.h"
+#include "WemosD1R1Pins.h"
 
-LcdKeypadShield::LcdKeypadShield(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7) : lcd(rs, enable, d4, d5, d6, d7)
+LcdKeypadShield::LcdKeypadShield(BoardType board)
 {
-    
+    switch (board)
+    {
+    case UNO:
+        LcdKeypadShield(8,9,4,5,6,7);
+        break;
+
+    case WemosD1R1:
+        LcdKeypadShield(D8,D9,D4,D5,D6,D7);
+        break;
+    }
+}
+
+LcdKeypadShield::LcdKeypadShield(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+{
+    this->lcd = new LiquidCrystal(rs, enable, d4, d5, d6, d7);
 }
 
 LcdKeypadShield::~LcdKeypadShield()
 {
+    if(this->lcd != nullptr)
+    {
+        delete this->lcd;
+    }
 }
--- Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.h
+++ Wemos-D1-LCD/lib/LcdKeypadShield/LcdKeypadShield.h
@@ -1,13 +1,18 @@
 #include <LiquidCrystal.h>
+#include "BoardType.h"
 
 class LcdKeypadShield
 {
 private:
 
-    LiquidCrystal lcd;
+    LiquidCrystal * lcd = nullptr;
 
 public:
 
+    //Connecting the shield directly to the board
+    LcdKeypadShield(BoardType board);
+
+    //Arbitrary connection of pins to LCD
     LcdKeypadShield(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
 
     ~LcdKeypadShield();
